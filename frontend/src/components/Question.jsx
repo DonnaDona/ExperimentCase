c47b4779c9a634b4e96dba15274cfbe7f8733875
Loredana De Filippo <defill@usi.ch>
2023-12-17 16:23:35 +0100

    frontend: Add green and red button.

--- frontend/src/components/Question.jsx
+++ frontend/src/components/Question.jsx
@@ -1,55 +1,67 @@
-import {useEffect, useState} from "react";
-import {Alert, Button, Card, CardContent, Stack, Typography} from "@mui/material";
+import { useEffect, useState } from "react";
+import { Alert, Button, Card, CardContent, Stack, Typography } from "@mui/material";
 
-export function Question({question, options, answer}) {
-
+export function Question({ question, options, answer }) {
     const [startTime, setStartTime] = useState(0);
     const [timeTaken, setTimeTaken] = useState(0);
+    const [selectedOption, setSelectedOption] = useState(null);
     const [correct, setCorrect] = useState(false);
-
 
     const getCurrentTime = () => {
         return window.performance.now();
-    }
+    };
 
     useEffect(() => {
+        // reset selectedOption and correct when a new question is displayed
+        setSelectedOption(null);
+        setCorrect(false);
+
         // start timing the new question
         setStartTime(getCurrentTime());
     }, [question]);
 
     const handleAnswerClick = (option) => {
         const endTime = getCurrentTime();
-        setTimeTaken((endTime - startTime));
+        setTimeTaken(endTime - startTime);
 
         if (option === answer) {
             console.log("Correct!");
             setCorrect(true);
         }
-    }
+
+        setSelectedOption(option);
+    };
 
     return (
-        <Card>
+        <Card sx={{ padding: 4 }}>
             <CardContent>
-                <Typography variant="h3" sx={{fontWeight: "bold"}}>{question}</Typography>
+                <Typography variant="h3" sx={{ fontWeight: "bold", marginBottom: 5 }}>
+                    {question}
+                </Typography>
                 <Stack>
-                    {options.map((option, index) => {
-                        return (
-                            <Button
-                                key={index}
-                                variant="outlined"
-                                size="large"
-                                sx={{marginTop: 3, fontSize: 26, textTransform: "none"}}
-                                onClick={() => {
-                                    handleAnswerClick(option);
-                                }}
-                            >
-                                {option}
-                            </Button>
-                        );
-                    })}
+                    {options.map((option, index) => (
+                        <Button
+                            key={index}
+                            variant="outlined"
+                            size="large"
+                            sx={{
+                                marginTop: 3,
+                                fontSize: 26,
+                                textTransform: "none",
+                                backgroundColor: selectedOption === option && correct ? "green" : selectedOption === option ? "red" : "transparent",
+                                color: selectedOption === option ? "white" : "black",
+                            }}
+                            onClick={() => {
+                                handleAnswerClick(option);
+                            }}
+                            disabled={selectedOption !== null}
+                        >
+                            {option}
+                        </Button>
+                    ))}
                 </Stack>
             </CardContent>
             {correct && <Alert severity="success">Correct!</Alert>}
         </Card>
-    )
+    );
 }
