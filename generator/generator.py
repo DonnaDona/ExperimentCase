2168c3c883f03f7d9f52848c9c22e04f57831cb0
Michele Dalle Rive <dallem@usi.ch>
2023-12-17 19:26:21 +0100

    Add random questions in warmup.

--- generator/generator.py
+++ generator/generator.py
@@ -47,8 +47,11 @@ def generate_right_answers_position(num_questions: int):
     return positions
 
 
-def generate_questions(words_list, words_per_question: int, num: int, answers_same_order=True):
+def generate_questions(words_list, words_per_question: int, num: int, answers_same_order=True,
+                       all_formats=True):
     questions = []
+    if not all_formats:
+        num *= 3
     right_answers_position = generate_right_answers_position(num)
     for i in range(num):
         words = extract_words(words_list, words_per_question)
@@ -56,6 +59,9 @@ def generate_questions(words_list, words_per_question: int, num: int, answers_sa
         # make sure that
         right_answer_position = right_answers_position[i]
         for j, format in enumerate(OPTIONS_FORMAT):
+            if not all_formats:
+                j = 0
+                format = random.choice(OPTIONS_FORMAT)
             question_obj = {"id": f"{i}-{j}", "question": format_words(words, "space"), "options": [], "format": format,
                             "answer": format_words(words, format)}
 
@@ -69,6 +75,9 @@ def generate_questions(words_list, words_per_question: int, num: int, answers_sa
 
             question_obj["options"] = options
             questions.append(question_obj)
+
+            if not all_formats:
+                break
 
     random.shuffle(questions)
 
@@ -92,7 +101,7 @@ def main(words_per_question, question_per_format, warmup_questions=1):
     with open("./words.txt", "r") as f:
         words_list = f.read().splitlines()
 
-        experiment["warmup"] = generate_questions(words_list, words_per_question, warmup_questions, False)
+        experiment["warmup"] = generate_questions(words_list, words_per_question, warmup_questions, False, False)
         experiment["questions"] = generate_questions(words_list, words_per_question, question_per_format)
 
     with open("./questions.json", "w") as f:
