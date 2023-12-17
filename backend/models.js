262ffe3b525cedbab0e1b5a2879c0364534908cd
Michele Dalle Rive <dallem@usi.ch>
2023-12-17 20:06:38 +0100

    Update schema to match frontend.

--- backend/models.js
+++ backend/models.js
@@ -1,30 +1,31 @@
 const mongoose = require('mongoose');
 
-const questionSchema = new mongoose.Schema({
-    question: {type: String, required: true},
-    textCase: {type: String, enum: ['camelCase', 'kebab-case', 'control'], required: true},
-    correct: {type: Boolean, required: true},
-    time: {type: Number, required: true}
+const answerSchema = new mongoose.Schema({
+    qid: {type: String, required: true},
+    answer: {type: String, required: true},
+    answerIndex: {type: Number, required: true},
+    time: {type: Number, required: true},
+    format: {type: String, required: true},
 });
-const Question = mongoose.model('Question', questionSchema);
+const Answer = mongoose.model('Question', answerSchema);
 
 const formSchema = new mongoose.Schema({
     age: {type: Number, required: true, min: [16, 'You must be at least 16 years old'], max: 110},
     eyeIssues: {type: Boolean, required: true},
     dyslexia: {type: Boolean, required: true},
-    nativeEnglish: {type: Boolean, required: true},
+    isNativeEnglishSpeaker: {type: Boolean, required: true},
     programmingExperience: {type: Number, required: true, min: 0, max: 50},
     languages: {
         type: [String],
-        required: true,
-        enum: ['C', 'C++', 'C#', 'CSS', 'HTML', 'Java', 'JavaScript', 'PHP', 'Python', 'Rust', 'Other']
+        enum: ['C', 'C++', 'C#', 'CSS', 'HTML', 'Java', 'JavaScript', 'PHP', 'Python', 'Rust', 'Other'],
+        default: [],
     },
 });
 const Form = mongoose.model('Form', formSchema);
 
 const experimentSchema = new mongoose.Schema({
-    questions: {type: [questionSchema], required: true}, form: {type: formSchema, required: true}
+    answers: {type: [answerSchema], required: true}, form: {type: formSchema, required: true}
 });
 const Experiment = mongoose.model('Experiment', experimentSchema);
 
-module.exports = {Question, Form, Experiment};
+module.exports = {Question: Answer, Form, Experiment};
