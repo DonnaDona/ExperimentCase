72ee8b777e4cad087733278b858e78508036e09c
Michele Dalle Rive <dallem@usi.ch>
2023-12-18 18:16:33 +0100

    Add random sorting of questions for each user.

--- frontend/src/questions.js
+++ frontend/src/questions.js
@@ -2,11 +2,14 @@ import questions_exp from "../../generator/questions.json";
 import demo_exp from "../../generator/demo.json";
 
 const formatQuestions = (exp) => [...exp["warmup"].map((q) => ({
-    ...q,
-    warmup: true
+    ...q, warmup: true
 })), ...exp["questions"].map((q) => ({...q, warmup: false}))];
 
 const questions = formatQuestions(questions_exp);
 const demo = formatQuestions(demo_exp);
+
+// shuffle
+questions.sort(() => Math.random() - 0.5);
+demo.sort(() => Math.random() - 0.5);
 
 export {questions, demo};
