6033cb7725da0040b4a753a591a492999d7873f5
Michele Dalle Rive <dallem@usi.ch>
2023-12-18 11:41:08 +0100

    Improve responsiveness of thank you page.

--- frontend/src/views/Finished.jsx
+++ frontend/src/views/Finished.jsx
@@ -1,4 +1,4 @@
-import {Card, CircularProgress, Stack, Typography} from "@mui/material";
+import {Card, CircularProgress, Stack, Typography, useTheme} from "@mui/material";
 import {useEffect, useState} from "react";
 import {selectExperiment} from "./experiment/experimentSlice.jsx";
 import {useSelector} from "react-redux";
@@ -10,6 +10,7 @@ export default function Finished() {
     const [error, setError] = useState(null);
     const {t} = useTranslation();
 
+    const theme = useTheme();
     const experiment = useSelector(selectExperiment);
 
     useEffect(() => {
@@ -22,29 +23,33 @@ export default function Finished() {
         }).finally(() => {
             setLoading(false);
         });
-    }, []);
+    }, [experiment]);
     return (<Card
         sx={{
             borderRadius: 6, padding: 5, width: 'min(650px, 100%)',
         }}>
         <Stack>
-            <Typography variant="h3" sx={{fontWeight: "bold", marginBottom: 2}}>
+            <Typography variant="h3" sx={{
+                fontWeight: "bold", marginBottom: 2, [theme.breakpoints.down('sm')]: {
+                    fontSize: 32,
+                }
+            }}>
                 {t("Thank you for participating!")}
             </Typography>
             {loading && <Stack justifyContent={'center'} alignItems={'center'}>
-                <Typography variant="h5" sx={{marginBottom: 2}}>
+                <Typography variant="subtitle1" sx={{marginBottom: 2}}>
                     {t("Sending your data... Please wait.")}
                 </Typography>
                 <CircularProgress/>
             </Stack>}
-            {error &&
-                <Stack justifyContent={'center'} alignItems={'center'}><Typography variant="h5" sx={{marginBottom: 2}}>
+            {error && <Stack justifyContent={'center'} alignItems={'center'}>
+                <Typography variant="subtitle1" sx={{marginBottom: 2}}>
                     {t("An error occurred while sending your data. Please notify the experiment conductors, reporting the following text:")}
                 </Typography>
-                    <textarea style={{width: "500px", height: "200px"}}
-                              value={`${error}\n\n${JSON.stringify(experiment)}`}/>
-                </Stack>}
-            {!loading && !error && <Typography variant="h5">
+                <textarea style={{width: "min(500px, 100%)", height: "200px"}}
+                          value={`${error}\n\n${JSON.stringify(experiment)}`}/>
+            </Stack>}
+            {!loading && !error && <Typography variant="subtitle1">
                 {t("Your data has been successfully sent. Thank you for participating!")}
             </Typography>}
         </Stack>
